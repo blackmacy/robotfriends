349f5b65b4b2cc8e550314a1d40819280f9b0b98
blacmacy <macyleeyijin95@gmail.com>
2019-11-28 00:58:46 -0800

    Updates

---
+++ precache-manifest.0d82328ff4aefea5ce6044c4ad9fa92f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12a263f9b0734bc1130e8902bfdce07f",
+    "url": "/robotfriends/index.html"
+  },
+  {
+    "revision": "32f624f541d3fd26fa06",
+    "url": "/robotfriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "d874b0134f6a40421cdf",
+    "url": "/robotfriends/static/css/main.282a307a.chunk.css"
+  },
+  {
+    "revision": "32f624f541d3fd26fa06",
+    "url": "/robotfriends/static/js/2.c2439eab.chunk.js"
+  },
+  {
+    "revision": "d874b0134f6a40421cdf",
+    "url": "/robotfriends/static/js/main.0fa63b45.chunk.js"
+  },
+  {
+    "revision": "8e2833c355870a1be621",
+    "url": "/robotfriends/static/js/runtime-main.e3489be3.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotfriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
